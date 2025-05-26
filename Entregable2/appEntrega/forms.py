4164d633491af21bad7b5dd2488e4f48f9e07ead
Xabierlb <xabierlb22@gmail.com>
2025-05-26 19:18:48 +0200

    fix: correcion componentes (#24)

--- Entregable2/appEntrega/forms.py
+++ Entregable2/appEntrega/forms.py
@@ -34,3 +34,6 @@ class ComponenteForm(forms.ModelForm):
     class Meta:
         model = Componente
         fields = ['codigo_referencia','nombre_modelo','tipo_componente','proveedor','precio','especificaciones_tecnicas','archivo']
+        widgets = {
+            'codigo_referencia': forms.TextInput(attrs={'class': 'mayusculas'})
+        }
